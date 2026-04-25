7bd116dfb96725a217a96c38eb67b70e283a69f2
git-mahmood <2025100000379@seu.edu.bd>
2026-04-25 15:58:56 +0600

    Adjust search bar container padding
    Updated the padding of the search bar container for better spacing.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -231,7 +231,7 @@ export default function Home() {
         </div>
 
         {/* ===== SEARCH BAR ===== */}
-        <div className="container mx-auto px-4 pt-6 pb-2" style={{ maxWidth:"900px" }}>
+        <div className="container mx-auto px-4 pt-10 pb-4" style={{ maxWidth:"900px" }}>
           <div className="relative">
             <svg className="absolute left-4 top-1/2 -translate-y-1/2 h-4 w-4" style={{ color:"rgba(0,200,255,0.5)",zIndex:2 }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-4.35-4.35M17 11A6 6 0 1 1 5 11a6 6 0 0 1 12 0z" /></svg>
             <input
